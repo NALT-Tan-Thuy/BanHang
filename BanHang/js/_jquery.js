29e6946ac18de7e993d95e0890797c1e8dba2c1c
tranquangtanqt <tranquangtanqt1990@gmail.com>
2018-07-22 23:55:09 +0700

    Thêm trở về đầu

---
+++ BanHang/js/_jquery.js
@@ -0,0 +1,47 @@
+var $button = $.backToTop({
+    // theme: 'fawesome',
+    // background color
+    backgroundColor: '#8b22a5cc',
+
+    // // bottom position
+    bottom: 50,
+
+    // // text color
+    color: '#FFFFFF',
+
+    // // container element
+    // container: this._body,
+
+    // // left or right
+    divFloat: 'right',
+
+    // // 'nonn', 'spin', 'fade', 'zoom', or 'spin-inverse'
+    effect: 'spin',
+
+    // // enable the back to top button
+    enabled: true,
+
+    // // width/height of the back to top button
+    height: 60,
+    width: 60,
+
+    // // icon
+    icon: 'fa fa-arrow-circle-up',
+
+    // // left position
+    left: 13,
+
+    // // trigger position
+    pxToTrigger: 600,
+
+    // // right position
+    right: 13,
+
+
+    // // or 'fawesome'
+    theme: 'fawesome',
+
+    // // z-index
+    zIndex: 999
+
+});
